028d71506670fb34517cf201b3fffc1dce6031bc
David Gethings <dgethings@juniper.net>
2013-01-08 16:29:53 +0000

    new snmp API using @dut.snmp.<method> format. Now consistent with Netconf API

--- lib/sloe.rb
+++ lib/sloe.rb
@@ -6,34 +6,16 @@ require 'snmp'
 module Sloe
 	class Device < Netconf::SSH
 
+		attr_reader :snmp
+
 	  def initialize(args)
 	    super(args)
 	    self.open
 
 	    @snmp_args = {:host => args[:target], :mib_dir => args[:mib_dir], :mib_modules => args[:mib_modules]}
-      @manager = SNMP::Manager.new(@snmp_args)
+      @snmp = SNMP::Manager.new(@snmp_args)
 	    
       self
-	  end
-
-	  def snmp_get(object)
-	  	@manager.get_value(object)
-	  end
-
-	  def snmp_get_pdu(object)
-	  	@manager.get(object)
-	  end
-
-	  def snmp_get_bulk(non_repeaters, max_repetitions, object_list)
-	  	@manager.get_bulk(non_repeaters, max_repetitions, object_list)
-	  end
-
-	  def snmp_get_next(object_list)
-	  	@manager.get_next(object_list)
-	  end
-
-	  def snmp_walk(object_list, index_column = 0, &block)
-	  	@manager.walk(object_list, index_column, &block)
 	  end
 	end
 end
--- spec/sloe_spec.rb
+++ spec/sloe_spec.rb
@@ -17,29 +17,29 @@ describe Sloe do
   end
 
   context "SNMP method" do
-    it "should return value for snmp_get()" do
-      @dut.snmp_get('sysDescr.0').should =~ /^Juniper Networks,/
+    it "should return value for snmp.get_value()" do
+      @dut.snmp.get_value('sysDescr.0').should =~ /^Juniper Networks,/
     end
-    it "should return PDU for snmp_get_pdu()" do
-      @pdu = @dut.snmp_get_pdu('sysDescr.0')
+    it "should return PDU for snmp.get()" do
+      @pdu = @dut.snmp.get('sysDescr.0')
       @pdu.varbind_list.should have(1).item
     end
-    it "should return list of PDUs for snmp_get_bulk()" do
-      @pdu = @dut.snmp_get_bulk(0, 5, 'system')
+    it "should return list of PDUs for snmp.get_bulk()" do
+      @pdu = @dut.snmp.get_bulk(0, 5, 'system')
       @pdu.varbind_list.should have(5).items
     end
-    it "should return PDU snmp_get_next()" do
-      @pdu = @dut.snmp_get_next('system')
+    it "should return PDU snmp.get_next()" do
+      @pdu = @dut.snmp.get_next('system')
       @pdu.varbind_list.should have(1).item
     end
-    it "should return list of PDUs for snmp_walk()" do
-      @dut.snmp_walk('system') { |vb| vb.should_not be_nil }
+    it "should return list of PDUs for snmp.walk()" do
+      @dut.snmp.walk('system') { |vb| vb.should_not be_nil }
     end
   end
 
   context "JNX Enterprise MIBs" do
     it "should have valid jnxBoxDescr value" do
-      @dut.snmp_get('jnxBoxDescr.0').should =~ /^Juniper/
+      @dut.snmp.get_value('jnxBoxDescr.0').should =~ /^Juniper/
     end
   end
 
