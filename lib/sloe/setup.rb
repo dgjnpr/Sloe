32859617a733f4ecdbfb7a3f03a8309f63dda94e
David Gethings <dgethings@juniper.net>
2013-05-20 18:59:49 +0100

    major structural change. Hope this works!

--- lib/sloe/setup.rb
+++ lib/sloe/setup.rb
@@ -1,11 +1,9 @@
 require 'celluloid'
-require 'net/netconf'
+require 'net/netconf/jnpr'
 require 'debugger'
 
 module Sloe
   class Setup
-
-    attr_reader :netconf, :hostname, :state
 
     def initialize( topology )
       raise Errno::ENOENT unless Dir.exists?( topology )
@@ -26,93 +24,75 @@ module Sloe
 
       Dir.glob( "#{@topology}/*.yaml" ) do |yaml_file|
         @hostname = File.basename yaml_file, '.yaml'
+        @junos = Dir.glob( "#{@topology}/#{@hostname}*.junos" )
 
-        # establish netconf connection to router
-        @login = {
-          :target   => @hostname,
-          :username => 'netconf',
-          :password => 'netconf'
-        }
-
-        @netconf = Netconf::SSH.new( @login )
-        @state   = :INCOMPLETE
-
-        @routers.push( self )
+        self.setup ( yaml_file, @junos )
       end
     end
 
-    def setup
+    def setup( @yaml, @junos )
       # debugger
-      @routers.each do |r|
-        # r.async._setup
-        r._setup
-      end
-      @state = :COMPLETE
-    end
+      @login = {
+        :target   => @hostname,
+        :username => 'netconf',
+        :password => 'netconf'
+      }
 
-    def complete
-      # @all_state = 0
-      # while @all_state < @routers.length do
-      #   @all_state = 0
-      #   @routers.each { |r| @all_state += 1 if r.state == :COMPLETE }
-      # end
-      @state == :COMPLETE ? true : false
+      @netconf = Netconf::SSH.new( @login )
+
+      @lab = File.read( "#{@location['template']}/lab/lab.conf" )
+      @config = []
+      @config.push({:config => @lab, :attrs => {:format => 'text', :action => 'merge'} })
+
+      @netconf.open unless @netconf.state == :NETCONF_OPEN
+
+      @specific = File.read( "#{@location['template']}/lab/#{self.hostname}.conf" )
+      @config.unshift({
+        :config => @specific, 
+        :attrs => { :format => 'text', :action => 'override' }
+      })
+      _apply_config( @config )
+      @config.shift
+      @config.shift
+
+      @junos.each do |file|
+        @ver = File.read( file )
+        _upgrade_junos( @ver )
+      end
+
+      @config.push ({
+        :config => _generate_config( "#{@location['template']}/#{self.hostname}.yaml" ),
+        :attrs  => { :format => 'text', :action => 'merge' }
+      })
+      _apply_config( @config )
+      @netconf.close
+
     end
 
     # private
-      def _setup
-        @lab = File.read( "#{@location['template']}/lab/lab.conf" )
-        @config = []
-        @config.push({:config => @lab, :attrs => {:format => 'text', :action => 'merge'} })
-
-        self.netconf.open unless self.netconf.state == :NETCONF_OPEN
-
-        @specific = File.read( "#{@location['template']}/lab/#{self.hostname}.conf" )
-        @config.unshift({
-          :config => @specific, 
-          :attrs => { :format => 'text', :action => 'override' }
-        })
-        _apply_config( @config )
-        @config.shift
-        @config.shift
-
-        if File.exist?( "#{@location['template']}/#{self.hostname}-re0.junos" )
-          @ver = File.read( "#{@location['template']}/#{self.hostname}-re0.junos" ).chomp
-          _upgrade_junos( @ver )
-        end
-
-        @config.push ({
-          :config => _generate_config( "#{@location['template']}/#{self.hostname}.yaml" ),
-          :attrs  => { :format => 'text', :action => 'merge' }
-        })
-        _apply_config( @config )
-        self.netconf.close
-        @state = :COMPLETE
-      end
-
       def _apply_config( config )
         begin
-          self.netconf.rpc.lock_configuration
+          @netconf.rpc.lock_configuration
           config.each do |conf|
-            self.netconf.rpc.load_configuration( conf[:config], conf[:attrs] )
+            @netconf.rpc.load_configuration( conf[:config], conf[:attrs] )
           end
-          self.netconf.rpc.commit_configuration
-          self.netconf.rpc.unlock_configuration
+          @netconf.rpc.commit_configuration
+          @netconf.rpc.unlock_configuration
         rescue Netconf::LockError => e
-          self.netconf.rpc.unlock_configuration
+          @netconf.rpc.unlock_configuration
           puts e.message
         rescue Netconf::EditError, Netconf::ValidateError, Netconf::CommitError, Netconf::RpcError => e
-          self.netconf.rpc.discard_changes
-          self.netconf.rpc.unlock_configuration
+          @netconf.rpc.discard_changes
+          @netconf.rpc.unlock_configuration
           puts e.message
         end
       end
 
       def _upgrade_junos( to_ver )
-        from_ver = self.netconf.rpc.get_system_information.xpath('//os-version').inner_text
+        from_ver = @netconf.rpc.get_system_information.xpath('//os-version').inner_text
 
         unless from_ver == to_ver
-          @re = self.netconf.rpc.get_route_engine_information.xpath('route-engine/mastership-state')
+          @re = @netconf.rpc.get_route_engine_information.xpath('route-engine/mastership-state')
           args = {
             :package_name => "ftp://orion/#{to_ver}/jinstall-#{to_ver}-domestic-signed.tgz",
             :reboot => true,
@@ -122,28 +102,28 @@ module Sloe
 
           # if dual RE perform upgrade on backup RE first
           if @re.size == 1
-            self.netconf.rpc.request_package_add( args )
+            @netconf.rpc.request_package_add( args )
           elsif @re[0].inner_text == "master"
             args[:re1] = true
-            self.netconf.rpc.request_package_add( args )
+            @netconf.rpc.request_package_add( args )
             args[:re0] = true
             args.delete(:re1)
-            self.netconf.rpc.request_package_add( args )
+            @netconf.rpc.request_package_add( args )
           else
             args[:re0] = true
-            self.netconf.rpc.request_package_add( args )
+            @netconf.rpc.request_package_add( args )
             args.delete(:re0)
             args[:re1] = true
-            self.netconf.rpc.request_package_add( args )
+            @netconf.rpc.request_package_add( args )
           end
 
           # close connection so we maintain same object while router reboots
-          self.netconf.close
+          @netconf.close
 
           sleep 1800 # give the RE 20 mins to upgrade
 
           # re-establish connection once upgrade complete
-          self.netconf.open
+          @netconf.open
         end
       end
 
